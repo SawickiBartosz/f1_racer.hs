7263341cd33b0c2f3bcdc381109be7d32964f226
SawickiBartosz <b.sawicki@student.mini.pw.edu.pl>
2022-05-05 12:39:09 +0200

    add collision detection

--- src/Main.hs
+++ src/Main.hs
@@ -5,7 +5,18 @@ import FRP.Yampa
 import Physics
 
 main :: IO ()
-main = do
-  putStrLn "Hello World"
+main = reactimate firstSample nextSamples consoleConsumer signalFunction
 
+firstSample :: IO Car
+firstSample = return spawnCar
 
+nextSamples :: Bool -> IO (DTime, Maybe Car)
+nextSamples _ = return (10, Nothing)
+
+consoleConsumer :: Bool -> Car -> IO Bool
+consoleConsumer _ x = do
+  print x
+  return False
+
+signalFunction :: SF Car Car
+signalFunction = arr (\car -> updatePosition $ updateVelocity car Up)
--- src/Physics.hs
+++ src/Physics.hs
@@ -6,14 +6,21 @@ import FRP.Yampa as Yampa
 
 
 data Car = Car {
-    p :: (V2 Float)
+    car_p :: (V2 Float)
    ,v :: (V2 Float)
+} deriving Show
+
+data Obstacle = Obstacle {
+    obstackle_p :: (V2 Float)
 } deriving Show
 
 data Direction = Up | Down | Right | Left
 
 updatePosition :: Car -> Car
 updatePosition (Car p v) = Car (p + v) v
+
+checkCollision :: Car -> Obstacle -> Bool
+checkCollision (Car p1 _) (Obstacle p2) = distance p1 p2 < 1
 
 hit :: Car -> Car
 hit (Car p _) = Car p (return 0 :: V2 Float)
