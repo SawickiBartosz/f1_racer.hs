88360aae46a013e878334336500937da02d49f0b
SawickiBartosz <b.sawicki@student.mini.pw.edu.pl>
2022-06-08 22:50:07 +0200

    changes in gui

--- src/Main.hs
+++ src/Main.hs
@@ -99,7 +99,8 @@ mergeWorlds (World [Car p1 v1 c1] obs fin [t1] _, World [Car p2 v2 c2] _ _ [t2] 
 
 
 renderIntro :: Picture
-renderIntro = translate (-200) 0 $ scale 0.2 0.2 $ text "Press any key to start the game"
+renderIntro = pictures $ [translate (-200) 0     $ scale 0.2 0.2 $ text "Press any key to start mulitplayer",
+                          translate (-180) (-40) $ scale 0.2 0.2 $ text "Press 1 to start singleplayer"]
 
 renderWorld :: World -> Picture
 renderWorld (World cars' obs fin _ Running) = translate 0 (-200) $ pictures $ (map renderCar cars') ++ 
@@ -116,9 +117,9 @@ renderCar (Car p v c) =  Color c $ translate (vector2X p) (vector2Y p) (ThickCir
 renderWinner :: [Time] -> [Picture]
 renderWinner [t1,t2]
   | t1 < t2 = [translate (-200) (-50) $ scale 0.3 0.3 $ Color red $ text $ "Player 1 won in " ++ showTime t1] ++
-              [translate (-200) (-80) $ scale 0.3 0.3 $ Color blue $ text $ "Player 2 lost: " ++ showTime t2]
+              [translate (-200) (-100) $ scale 0.3 0.3 $ Color blue $ text $ "Player 2 lost: " ++ showTime t2]
   | t1 > t2 = [translate (-200) (-50) $ scale 0.3 0.3 $ Color blue $ text $ "Player 2 won in " ++ showTime t2] ++
-              [translate (-200) (-80) $ scale 0.3 0.3 $ Color red $ text $ "Player 1 lost: " ++ showTime t1]
+              [translate (-200) (-100) $ scale 0.3 0.3 $ Color red $ text $ "Player 1 lost: " ++ showTime t1]
   | otherwise = [translate (-200) (-50) $ scale 0.3 0.3 $ text $ "Draw in time: " ++ showTime t2]
 
 renderObstacle :: Obstacle -> Picture
