659ce2e4d1e3471ef550c9e7b476f3cd98cfd52b
SawickiBartosz <b.sawicki@student.mini.pw.edu.pl>
2022-06-09 12:09:36 +0200

    add docs

--- src/Render.hs
+++ src/Render.hs
@@ -8,10 +8,12 @@ import Types
 import Graphics.Gloss
 import Data.Vector2
 
+-- | Render initial picture of a game
 renderIntro :: Picture
 renderIntro = pictures $ [translate (-200) 0     $ scale 0.2 0.2 $ text "Press any key to start mulitplayer",
                           translate (-180) (-40) $ scale 0.2 0.2 $ text "Press 1 to start singleplayer"]
 
+-- | Render World
 renderWorld :: World -> Picture
 renderWorld (World cars' obs fin _ Running) = translate 0 (-200) $ pictures $ (map renderCar cars') ++ 
                                                          (map renderObstacle obs) ++
@@ -21,9 +23,11 @@ renderWorld (World [_] _ _ ts Finished) = pictures $ [translate (-100) 0 $ scale
 
 renderWorld (World [_,_] _ _ ts Finished) = pictures $ [translate (-100) 0 $ scale 0.4 0.4 $ text $ "Finished!"] ++ (renderWinner ts)
 
+-- | Render Car
 renderCar :: Car -> Picture
 renderCar (Car p v c) =  Color c $ translate (vector2X p) (vector2Y p) (ThickCircle 4 7)
 
+-- | Based on times render end results of 2 players
 renderWinner :: [Time] -> [Picture]
 renderWinner [t1,t2]
   | t1 < t2 = [translate (-200) (-50) $ scale 0.3 0.3 $ Color red $ text $ "Player 1 won in " ++ showTime t1] ++
@@ -32,21 +36,26 @@ renderWinner [t1,t2]
               [translate (-200) (-100) $ scale 0.3 0.3 $ Color red $ text $ "Player 1 lost: " ++ showTime t1]
   | otherwise = [translate (-200) (-50) $ scale 0.3 0.3 $ text $ "Draw in time: " ++ showTime t2]
 
+-- | Render Obstacle
 renderObstacle :: Obstacle -> Picture
 renderObstacle (Obstacle p s _) = polygon $ pathFromPosSize p s
 
+-- | Render finish spot
 renderFinish :: Finish -> Picture
 renderFinish (Obstacle p s _) = Color red $ polygon $ pathFromPosSize p s
 
-pathFromPosSize :: Pos -> Vector2 Float -> Path
+-- | Helper function to make Gloss Path from position and size
+pathFromPosSize :: Pos -> Size -> Path
 pathFromPosSize p s = [(vector2X p, vector2Y p), 
                        (vector2X p + vector2X s, vector2Y p),
                        (vector2X p + vector2X s, vector2Y p + vector2Y s), 
                        (vector2X p , vector2Y p + vector2Y s)]
 
+-- | Helper function to format printed time
 showTime :: Time -> String
 showTime t = show $ truncate' t 3
 
+-- | Round number to specified decimal places
 truncate' :: Double -> Int -> Double
 truncate' x n = (fromIntegral (floor (x * t))) / t
     where t = 10^n
--- src/World.hs
+++ src/World.hs
@@ -1,17 +1,22 @@
 module World where
 
-
 import FRP.Yampa
 import Types
 import Graphics.Gloss
 
+-- | make constant SF from World, used to snapshot state afertr finish
 fromWorld :: World -> SF a World
 fromWorld (World cars' obs fin t s) = constant (World cars' obs fin t s)
 
-makeWorld :: Color ->  ((Pos, Vel, [Obstacle], Finish, GameState), Time) -> (World, Event World)
+-- | Make World from individual components
+-- Always returns World and if game finished also Event of World
+makeWorld :: Color -- ^ color of player
+            -> ((Pos, Vel, [Obstacle], Finish, GameState), Time)
+            -> (World, Event World)
 makeWorld c ((p,v, obs, fin, Running), t) = (World ([Car p v c]) obs fin [t] Running, NoEvent)
 makeWorld c ((p,v, obs, fin, Finished), t) = (World ([Car p v c]) obs fin [t] Finished, Event (World ([Car p v c]) obs fin [t] Finished))
 
+-- | Join worlds of each player into one
 mergeWorlds :: (World, World) -> World
 mergeWorlds (World [Car p1 v1 c1] obs fin [t1] Running, World [Car p2 v2 c2] _ _ [t2] Running)   = World ([Car p1 v1 c1, Car p2 v2 c2]) obs fin [t1,t2] Running
 mergeWorlds (World [Car p1 v1 c1] obs fin [t1] Finished, World [Car p2 v2 c2] _ _ [t2] Finished) = World ([Car p1 v1 c1, Car p2 v2 c2]) obs fin [t1,t2] Finished
